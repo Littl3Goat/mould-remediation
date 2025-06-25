eeadd60f2e6f0cb4a9dc6c73fbc45ac88040111d
Bao Ho <hhqbao@gmail.com>
2025-06-25 12:27:26 +1000

    Add carousel to display work comparision

--- MouldRemediation/mouldremediation.client/src/app/components/contact-us/contact-us.component.ts
+++ MouldRemediation/mouldremediation.client/src/app/components/contact-us/contact-us.component.ts
@@ -1,11 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: 'contact-us.component.html',
+  imports: [ReactiveFormsModule],
 })
 export class ContactUsComponent implements OnInit {
-  constructor() {}
+  contactForm: FormGroup;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.contactForm = new FormGroup({
+      name: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required]),
+      phone: new FormControl(null, [Validators.required]),
+      service: new FormControl(null, []),
+      message: new FormControl(null, [Validators.required]),
+    });
+  }
+
+  onSendMessage = () => {
+    if (this.contactForm.invalid) return;
+
+    console.log(this.contactForm.value);
+  };
 }
--- MouldRemediation/mouldremediation.client/src/app/components/why-choose-us/why-choose-us.component.ts
+++ MouldRemediation/mouldremediation.client/src/app/components/why-choose-us/why-choose-us.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageCompareComponent } from '../image-compare/image-compare.component';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-why-choose-us',
-
   templateUrl: 'why-choose-us.component.html',
   standalone: true,
+  imports: [ImageCompareComponent, SlickCarouselModule],
 })
 export class WhyChooseUsComponent implements OnInit {
-  constructor() {}
+  slideConfig = {
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    draggable: false,
+    dots: true,
+    prevArrow: `<button type="button" class="slick-prev">
+                  <i class="fa-solid fa-circle-chevron-left"></i>
+               </button>`,
+    nextArrow: `<button type="button" class="slick-next">
+                  <i class="fa-solid fa-circle-chevron-right"></i>
+                </button>`,
+  };
 
   ngOnInit() {}
 }
