2329e3a4502dad6c9585e732c76ffd92d5e82d53
Bao Ho <hhqbao@gmail.com>
2025-06-26 21:06:00 +1000

    Added scrolling for each button and added company logo.

--- MouldRemediation/mouldremediation.client/src/app/components/footer/footer.component.ts
+++ MouldRemediation/mouldremediation.client/src/app/components/footer/footer.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
   templateUrl: 'footer.component.html',
   standalone: true,
+  imports: [ReactiveFormsModule],
 })
 export class FooterComponent implements OnInit {
   constructor() {}
 
   today = new Date();
 
-  ngOnInit() {}
+  emailNewsForm: FormGroup;
+
+  ngOnInit() {
+    this.emailNewsForm = new FormGroup({
+      emailNews: new FormControl(null, [Validators.required]),
+    });
+  }
+
+  onSendEmail = () => {
+    if (this.emailNewsForm.invalid) return;
+  };
 }
